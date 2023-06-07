9b72f6c38d2540e43be8f17722f0c3a6b90d42f2
viniciussecari <viniciussecari987@gmail.com>
2023-06-07 19:32:13 -0300

    :bulb: Add Experiences section

--- src/locales/en.js
+++ src/locales/en.js
@@ -2,6 +2,7 @@ export const en = {
   profile: {
     position: 'FrontEnd Developer',
   },
+  sections: { experiences: { title: 'Experiences', now: 'Now' } },
   message: {
     hello: 'Hello',
   },
---
+++ src/utils/constants/EXPERIENCES.js
@@ -0,0 +1,73 @@
+export const EXPERIENCES = [
+  {
+    title: 'Confidential',
+    desc: `Atuando como desenvolvedor FrontEnd, desenvolvendo novas features, 
+            resolvendo bugs, criando e refatorando testes unitários seguindo metodologia Scrum. 
+            Participo de reuniões pontualmente para discutir sobre arquitetura do nosso produto`,
+    position: 'Desenvolvedor FrontEnd Pleno',
+    initialDatetime: '07/2022',
+    finalDatetime: '',
+    tools: ['VueJs', 'Vuetify', 'Vuex', 'VueRouter', 'Scrum', 'Agile', 'JSON'],
+  },
+  {
+    title: 'FarmTech',
+    desc: `Atuando como desenvolvedor FrontEnd, desenvolvendo novas features e 
+            resolvendo bugs utilizando metodologia Scrum. 
+            Participo de reuniões pontualmente para discutir sobre arquitetura do nosso produto, 
+            desenvolvendo testes e2e (End-To-End) e testes unitários com Jest`,
+    position: 'Desenvolvedor FrontEnd Júnior',
+    initialDatetime: '05/2022',
+    finalDatetime: '07/2022',
+    tools: [
+      'VueJs',
+      'Vuetify',
+      'Vuex',
+      'VueRouter',
+      'Cypress',
+      'JSON',
+      'Mobile First',
+    ],
+  },
+  {
+    title: 'FarmTech',
+    desc: `Atuando como desenvolvedor FrontEnd, desenvolvendo novas features e 
+            resolvendo bugs utilizando metodologia Scrum. 
+            Participo de reuniões pontualmente para discutir sobre arquitetura do nosso produto, 
+            desenvolvendo testes e2e (End-To-End) e testes unitários com Jest`,
+    position: 'Estagiário de Desenvolvimento FrontEnd',
+    initialDatetime: '07/2021',
+    finalDatetime: '05/2022',
+    tools: [
+      'VueJs',
+      'Vuetify',
+      'Vuex',
+      'VueRouter',
+      'Cypress',
+      'JSON',
+      'Mobile First',
+    ],
+  },
+  {
+    title: 'Inking',
+    desc: `Atuava criando novas features, resolução de bugs e criação de componentes com Vue.Js, 
+            desenvolvimento de APIS em Java(SpringBoot) e 
+            manutenção/atualização do banco de dados em MySQL e 
+            também realizando ocasionalmente discussões de UI e UX.`,
+    position: 'Desenvolvedor de software FullStack júnior',
+    initialDatetime: '09/2020',
+    finalDatetime: '07/2021',
+    tools: [
+      'VueJs',
+      'Vuetify',
+      'Vuex',
+      'VueRouter',
+      'Cypress',
+      'JSON',
+      'Mobile First',
+      'Java',
+      'Spring Boot',
+      'MySQL',
+      'Swagger',
+    ],
+  },
+];
