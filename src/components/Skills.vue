0496946d214934f584a32fa121bd765fa14b4a12
viniciussecari <viniciussecari987@gmail.com>
2023-06-13 19:16:28 -0300

    :repeat: Fix svgs imports

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -11,6 +11,15 @@
 </template>
 
 <script setup>
+import js from '../assets/icons/js.svg';
+import git from '../assets/icons/git.svg';
+import vue from '../assets/icons/vue.svg';
+import code from '../assets/icons/code.svg';
+import jest from '../assets/icons/jest.svg';
+import css_3 from '../assets/icons/css-3.svg';
+import html_5 from '../assets/icons/html-5.svg';
+import cypress from '../assets/icons/cypress.svg';
+
 import Skill from './Skill.vue';
 
 const skills = [
@@ -18,51 +27,51 @@ const skills = [
     title: 'Vue JS',
     description: `Vue.js é um framework JavaScript de código-aberto, 
         focado no desenvolvimento de interfaces de usuário e aplicativos de página única`,
-    icon: '../assets/icons/vue.svg',
+    icon: vue,
   },
   {
     title: 'HTML',
     description:
       'HTML é uma linguagem de marcação utilizada na construção de páginas na Web.',
-    icon: '../assets/icons/html-5.svg',
+    icon: html_5,
   },
   {
     title: 'CSS',
     description: `Cascading Style Sheets é um mecanismo para adicionar estilos a uma página web, 
         aplicado diretamente nas tags HTML ou ficar contido dentro das tags <style>.`,
-    icon: '../assets/icons/css-3.svg',
+    icon: css_3,
   },
   {
     title: 'JS',
     description: `JavaScript é uma linguagem de programação interpretada estruturada, 
         de script em alto nível com tipagem dinâmica fraca e multiparadigma.`,
-    icon: '../assets/icons/js.svg',
+    icon: js,
   },
   {
     title: 'Jest',
     description: `Jest é uma estrutura de teste de JavaScript 
     construída sobre o Jasmine e mantida pela Meta.`,
-    icon: '../assets/icons/jest.svg',
+    icon: jest,
   },
   {
     title: 'Cypress',
     description: `Cypress é uma ferramenta de teste de front-end 
     para aplicativos da web.`,
-    icon: '../assets/icons/cypress.svg',
+    icon: cypress,
   },
   {
     title: 'Git',
     description: `Git é um sistema de controle de versões distribuído, 
     usado principalmente no desenvolvimento de software, mas pode ser usado para registrar o
     histórico de edições de qualquer tipo de arquivo.`,
-    icon: '../assets/icons/git.svg',
+    icon: git,
   },
   {
     title: 'Clean Code',
     description: `Clean Code ou código limpo se refere a um conjunto de boas práticas na 
     escrita de software que você pode aplicar para obter uma maior legibilidade 
     e manutenabilidade do seu código.`,
-    icon: '../assets/icons/code.svg',
+    icon: code,
   },
 ];
 </script>
