e31b448fb5949cb69706b0a825f53ed27d7d222c
viniciussecari <viniciussecari987@gmail.com>
2023-07-14 15:54:43 -0300

    :bug: Fix college name to right name

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -6,7 +6,7 @@
 
     <div class="my-2">
       <h2 class="mb-2 font-light text-2xl">
-        Universidade Presbeteriana Mackenzie
+        Universidade Presbiteriana Mackenzie
       </h2>
       <h3>Análise e desenvolvimento de Sistemas</h3>
 
