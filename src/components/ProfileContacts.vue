b6373861d99614d03d40a3e536d73f2e5970a634
viniciussecari <viniciussecari987@gmail.com>
2023-07-14 16:18:51 -0300

    :sparkles: Add new ways to contact me

---
+++ src/components/ProfileContacts.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{ $t('profile.position') }}</h2>
+    <h2>{{ $t('profile.location') }}</h2>
+    <h2
+      class="px-2 hover:transition-shadow hover:shadow-lg hover:shadow-cyan-500/50">
+      <a href="+5511989021001">{{ $t('profile.phone') }}</a>
+    </h2>
+    <h2
+      class="px-2 hover:transition-shadow hover:shadow-lg hover:shadow-cyan-500/50">
+      <a href="mailto:viniciussecari987@gmail.com">{{ $t('profile.email') }}</a>
+    </h2>
+  </div>
+</template>
+
+<script setup></script>
--- src/locales/pt_BR.js
+++ src/locales/pt_BR.js
@@ -2,6 +2,9 @@ export const pt_BR = {
   profile: {
     createdBy: 'Feito por: Vinicius Secari',
     position: 'Desenvolvedor FrontEnd',
+    location: 'São Paulo - Brasil',
+    phone: '+55 11 98902-1001',
+    email: 'viniciusecari987@gmail.com',
   },
   sections: {
     experiences: {
